9f081f46d093c5b126f5271426d2bfedb76ec774
Looper <aayush.bhattacharjee2002@gmail.com>
2023-06-16 19:47:16 +0530

    Started working on the project

--- app.mjs
+++ app.mjs
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+import db from "./server.mjs";
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.set("view engine", "ejs");
+app.use("views", path.join(__dirname, "/Views"));
+app.get("/", (req, res) => {
+  res.send(db.ping());
 });
-
 app.listen(8000, () => {
   console.log("Server started on port 8000");
 });
