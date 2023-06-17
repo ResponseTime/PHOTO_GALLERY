7dabfb72100965e9b533a1593f87dc8b880ff77f
Looper <aayush.bhattacharjee2002@gmail.com>
2023-06-17 23:41:12 +0530

    Started working on the client interface

---
+++ Client/app.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+app = Tk()
+app.geometry("1600x800")
+frame = ttk.Frame(app,padding=10)
+frame.pack()
+ttk.Label(frame,text="Hello World").grid(column=0,row=0)
+app.mainloop()
